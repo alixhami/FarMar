04ab6b4696b734cbc4d20d1316a4757ce3b37fef
Alix Hamilton <alix.hamilton@gmail.com>
2017-03-01 09:27:01 -0800

    adds simplecov and gitignore

--- specs/spec_helper.rb
+++ specs/spec_helper.rb
@@ -1,5 +1,10 @@
+require 'simplecov'
+SimpleCov.start
+
 require 'minitest/autorun'
 require 'minitest/reporters'
 require 'minitest/skip_dsl'
 
 require_relative '../far_mar'
+
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
