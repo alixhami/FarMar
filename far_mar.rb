5da529acdcdd822828ba404f335fe15e6fc02610
Alix Hamilton <alix.hamilton@gmail.com>
2017-02-28 20:19:22 -0800

    adds project skeleton

---
+++ far_mar.rb
@@ -0,0 +1,7 @@
+require 'csv'
+
+module FarMar; end
+require 'lib/farmar_market'
+require 'lib/product'
+require 'lib/sale'
+require 'lib/vendor'
---
+++ specs/spec_helper.rb
@@ -0,0 +1,5 @@
+require 'minitest/autorun'
+require 'minitest/reporters'
+require 'minitest/skip_dsl'
+
+require_relative '../far_mar'
